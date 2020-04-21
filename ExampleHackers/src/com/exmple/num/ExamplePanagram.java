a67681fa0685449a288500d12b5ecb0129bf8f20
srinivas <srinivas.tutigunta@gmail.com>
2020-04-21 19:56:35 +0530

    commit3

--- ExampleHackers/src/com/exmple/num/ExamplePanagram.java
+++ ExampleHackers/src/com/exmple/num/ExamplePanagram.java
@@ -3,35 +3,34 @@ package com.exmple.num;
 public class ExamplePanagram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 String str = "The quick brown fox jumps over the lazy dog"; 
-		  
-	        if (checkPangram(str) == true) 
-	            System.out.print(str + " is a pangram."); 
-	        else
-	            System.out.print(str + " is not a pangram."); 
-	    }
-
-	private static boolean checkPangram(String str) {
-		
-		boolean[] mark = new boolean[26]; 
-	    int index = 0; 
-     for (int i = 0; i < str.length(); i++) { 
-            if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') 
-                index = str.charAt(i) - 'A'; 
-             else if ('a' <= str.charAt(i) && str.charAt(i) <= 'z') 
-                  index = str.charAt(i) - 'a'; 
-             else
-                continue; 
-            mark[index] = true; 
-        } 
-  
-       
-        for (int i = 0; i <= 25; i++) 
-            if (mark[i] == false) 
-                return (false); 
-            return (true); 
-	} 
+		String str = "The quick brown fox ju0ps over the lazy dog";
+		System.out.println(findMissingLetters(str));
 	}
 
+	public static String findMissingLetters(String mySentence) {
+		String result = "";
+		char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+				'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+				'w', 'x', 'y', 'z' };
 
+		String missingLetters = "";
+		int countMissLetters = 0;
+
+		for (int i = 0; i < alphabet.length; i++) {
+			if (!mySentence.toLowerCase().contains(
+					String.valueOf(alphabet[i]).toLowerCase())) {
+				missingLetters = missingLetters + String.valueOf(alphabet[i])
+						+ " ";
+				countMissLetters++;
+			}
+		}
+		if (countMissLetters == 0)
+			System.out.println("Sting as  Panagram ");
+		else
+			System.out.println("Sting as  Not Panagram ");
+		result = "Letters who are missing: " + missingLetters + "Counted: "
+				+ countMissLetters;
+		return result;
+	}
+
+}
