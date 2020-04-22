bac3392295128ec07e395aa79c5eabab6818391b
srinivas <srinivas.tutigunta@gmail.com>
2020-04-22 20:05:56 +0530

    commit 6

---
+++ ExampleHackers/src/com/exmple/num/LotteryCouponsAppl.java
@@ -0,0 +1,45 @@
+package com.exmple.num;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class LotteryCouponsAppl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the input");
+		int noOfLotteries = input.nextInt();
+		Map<Integer, Integer> allSumMap = new HashMap<>();
+		for (int lotorryNum = 1; lotorryNum <= noOfLotteries; lotorryNum++) {
+			int lottorySum = getLotterySum(lotorryNum);
+			if (allSumMap.containsKey(lottorySum)) {
+				allSumMap.put(lottorySum,
+						(new Integer(lottorySum).intValue() + 1));
+			} else {
+				allSumMap.put(lottorySum, new Integer(1));
+			}
+		}
+		// System.out.println(allSumMap);
+		int maxValueInMap = (Collections.max(allSumMap.values()));
+		for (Entry<Integer, Integer> entry : allSumMap.entrySet()) {
+			if (entry.getValue() == maxValueInMap) {
+				System.out.println(entry.getKey());
+			}
+		}
+	}
+
+	private static int getLotterySum(int lotteryNum) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		while (lotteryNum != 0) {
+			sum = sum + lotteryNum % 10;
+			lotteryNum = lotteryNum / 10;
+		}
+		return sum;
+	}
+
+}
